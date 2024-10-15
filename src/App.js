52bce5b20e6672e76355c0616e23c3e60f10d9be
khachikkar <khachikkarapetyan78@gmail.com>
2024-10-15 12:41:45 +0400

    try to fix last city nam show bag

--- src/App.js
+++ src/App.js
@@ -1,128 +1,125 @@
-import React, {  useEffect, useState } from 'react';
-import { API_KEY } from './constants/constants';
-import {Button, Flex, Form, Input} from "antd"
+import React, { useEffect, useState } from "react";
+import { API_KEY } from "./constants/constants";
+import { Button, Flex, Form, Input } from "antd";
 
+import "./App.css";
+import "./core/index.css";
 
-
-
-import './App.css';
-import "./core/index.css"
-
-
-let mycities = new Set()
-while(mycities.size < 5){
-  mycities.add(Math.floor(Math.random()*20))
+let mycities = new Set();
+while (mycities.size < 5) {
+  mycities.add(Math.floor(Math.random() * 20));
 }
-
 
 function App() {
+  const [form] = Form.useForm();
 
-const [form] = Form.useForm()
+  const cities = [
+    "New York",
+    "Los Angeles",
+    "London",
+    "Paris",
+    "Tokyo",
+    "Sydney",
+    "Berlin",
+    "Moscow",
+    "Rome",
+    "Dubai",
+    "Beijing",
+    "Toronto",
+    "Mumbai",
+    "Mexico City",
+    "Yerevan",
+    "Seoul",
+    "Cape Town",
+    "Buenos Aires",
+    "Hong Kong",
+    "Singapore",
+  ];
+  // const [city, setCity] =useState(cities[0])
+
+  let gamenums = [...mycities];
+  console.log(gamenums);
+  const [count, setCount] = useState(0);
+  const [mycity, setMycity] = useState(cities[gamenums[count]]);
+  const [temp, setTemp] = useState(0);
+
+  useEffect(() => {
+    try{
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${mycity}&appid=${API_KEY}`;
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setTemp(Math.floor(data.main.temp - 273.15));
+      });
+    }catch(e){
+      console.log(e)
+    }
+  }, [mycity]);
+
+  const handleInput = async (value) => {
+    const newCount = count + 1;
+
+    //////// fix this bag part /////
+    if (newCount === 6 ){
+     setMycity("Yerevan")
+      return
+    }
+
+    setCount(newCount);
+    setMycity(cities[gamenums[newCount]]);
+
+    const { search } = value;
+
+    form.resetFields();
+
+    console.log(search, ">>>>");
+    console.log(mycity, ">>>>");
+    console.log(temp, ">>>>");
+   console.log( handleCheck(search, temp) ? "win" : "lose", "XXXXXXX")
+  };
 
 
-const cities = [
-  "New York",
-  "Los Angeles",
-  "London",
-  "Paris",
-  "Tokyo",
-  "Sydney",
-  "Berlin",
-  "Moscow",
-  "Rome",
-  "Dubai",
-  "Beijing",
-  "Toronto",
-  "Mumbai",
-  "Mexico City",
-  "Yerevan",
-  "Seoul",
-  "Cape Town",
-  "Buenos Aires",
-  "Hong Kong",
-  "Singapore"
-];
-// const [city, setCity] =useState(cities[0])
+  const handleCheck = (search, temp)=>{
+    let min = temp-4
+    let max = temp + 4
 
+    if(search >= min && search <= max){
+      return true
+    }else{
+      return false
+    }
+  }
 
-
-let gamenums = [...mycities]
-console.log(gamenums)
-const [count, setCount] = useState(0)
-const [mycity, setMycity] =useState(cities[gamenums[count]])
-const [temp, setTemp] = useState(0)
-
-useEffect(()=>{
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${mycity}&appid=${API_KEY}`
-  fetch(url)
-  .then(resp=>resp.json())
-  .then(data=> {
-    setTemp(Math.floor(data.main.temp-273.15) )
-  })
-}, [mycity])
-
-const handleInput = async (value)=>{
-
-  const newCount = (count + 1)
-
-  if(newCount === 5) return
-
-  setCount(newCount)
-  setMycity(cities[gamenums[newCount]])
-
-
-  const {search} = value
-
-  form.resetFields()
-
-  console.log(search, ">>>>")
-  console.log(mycity, ">>>>")
-  console.log(temp, ">>>>")
-  
-
-
-///// fetch part
-
-
-
-}
 
 
   return (
     <div className="App">
+      <h1>GameWe</h1>
 
-<h1>GameWe</h1>
+      <div className="container">
+        <span>Game created by Khach</span>
 
-    <div className='container'>
-     
-<span>Game created by Khach</span>
+        <Flex vertical gap="small">
+          <h2>{mycity}</h2>
+          <Form layout="vertical" form={form} onFinish={handleInput}>
+            <Form.Item name="search">
+              <Input placeholder="enter a city name" name="search" />
+            </Form.Item>
 
-<Flex vertical gap="small">
-<h2>{mycity}</h2>
-  <Form layout='vertical' form={form} onFinish={handleInput} >
+            <Button htmlType="submit" className="button" type="primary">
+              Check
+            </Button>
+          </Form>
+        </Flex>
 
-<Form.Item
- name='search'
- >
-<Input placeholder='enter a city name'  name='search'/>
-</Form.Item>
-
-<Button htmlType='submit' className="button" type='primary'>Check</Button>
-
-  </Form>
-
-</Flex>
-
-<Flex layout="horizonatal" gap="small">
-<div className='round'></div>
-<div className='round'></div>
-<div className='round'></div>
-<div className='round'></div>
-<div className='round'></div>
-</Flex>
-
-    </div>
-
+        <Flex layout="horizonatal" gap="small">
+          <div className="round"></div>
+          <div className="round"></div>
+          <div className="round"></div>
+          <div className="round"></div>
+          <div className="round"></div>
+        </Flex>
+      </div>
     </div>
   );
 }
